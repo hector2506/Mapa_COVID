9f0d07b7a2bbf592b67e3b0f4bc65270bae500e8
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-06-09 20:38:51 -0300

    colocando app_name em todos os apps

--- patient/urls.py
+++ patient/urls.py
@@ -3,6 +3,8 @@ from django.urls import path,include
 from .views import *
 from django.contrib.auth import views as auth_views
 
+app_name = "patient"
+
 urlpatterns = [
     path('lista',paciente_list, name='home'),
     path('novo-paciente/',novo_paciente, name='novo_paciente'),
--- site_mapa/urls.py
+++ site_mapa/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from site_mapa.views import *
 from django.contrib.auth import views as auth_views
 
+app_name = "site_mapa"
 urlpatterns = [
 
     path('mapa/',mapa, name='mapa'),
